72764845be72a4a5a05c78daba7bf89bb2da4987
dbaldew <dbaldew@gmail.com>
2021-09-23 11:24:08 +0200

    v1

--- src/App.js
+++ src/App.js
@@ -5,17 +5,30 @@ import CavitiesPage from './pages/Cavities';
 import AppointmentsPage from './pages/Appointments';
 import WhiteningPage from './pages/Whitening';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
-  return (
-    <>
-      <TopMenu />
-      <HomePage />
-      <CavitiesPage />
-      <AppointmentsPage />
-      <WhiteningPage />
-    </>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route>
+                    <TopMenu/>
+                </Route>
+                <Route exact path="/">
+                    <HomePage/>
+                </Route>
+                <Route exact path="/gaatjes">
+                    <CavitiesPage/>
+                </Route>
+                <Route exact path="/afspraak">
+                    <AppointmentsPage/>
+                </Route>
+                <Route exact path="/tanden-bleken">
+                    <WhiteningPage/>
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
